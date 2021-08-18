de91dcede636244885e56b1a3011cb90d1a2f7f0
mananjainiitr <76247872+mananjainiitr@users.noreply.github.com>
2021-08-18 21:47:35 +0530

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -4,20 +4,14 @@ from django.db import models
 class TodoList(models.Model):
 
     list_name = models.CharField(max_length=100)
-    
-    
-
     def __str__(self):
         return f"{self.list_name}"
 
 class TodoItem(models.Model):
 
     title = models.CharField(max_length=100)
-    # checked = models.BooleanField(default=False)
-    due_date = models.DateField()
-    due_tim = models.CharField(max_length=20,default="Not specified")
-
+    checked = models.BooleanField(default=False)
+    due_date = models.DateTimeField()
     todo_list = models.ForeignKey(to=TodoList, on_delete=models.CASCADE)
-
     def __str__(self):
         return f"{self.todo_list.list_name}: {self.title} "
